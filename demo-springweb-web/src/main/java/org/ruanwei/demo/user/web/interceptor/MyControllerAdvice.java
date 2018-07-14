45bc05bb00baa05f628ff769ec6a085fb00b4ec6
ruanwei <ruanwei@192.168.0.4>
2018-07-14 15:23:05 +0800

    Signed-off-by: ruanwei <ruanwei@192.168.0.4>

--- demo-springweb-web/src/main/java/org/ruanwei/demo/user/web/interceptor/MyControllerAdvice.java
+++ demo-springweb-web/src/main/java/org/ruanwei/demo/user/web/interceptor/MyControllerAdvice.java
@@ -29,7 +29,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
