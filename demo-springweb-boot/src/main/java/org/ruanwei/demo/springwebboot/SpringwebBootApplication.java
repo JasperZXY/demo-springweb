79d654edcf06647849a2e6d82263f371fdb4328d
ruanwei <ruanwei@10.12.1.49>
2018-07-15 17:40:44 +0800

    Signed-off-by: ruanwei <ruanwei@10.12.1.49>

---
+++ demo-springweb-boot/src/main/java/org/ruanwei/demo/springwebboot/SpringwebBootApplication.java
@@ -0,0 +1,12 @@
+package org.ruanwei.demo.springwebboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebBootApplication.class, args);
+	}
+}
