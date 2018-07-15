b2e24bf9f43f4a22ebe99c026cf61fb3f6b19c9e
ruanwei <ruanwei@10.12.1.49>
2018-07-15 18:16:50 +0800

    Signed-off-by: ruanwei <ruanwei@10.12.1.49>

--- demo-springweb-boot/src/main/java/org/ruanwei/demo/user/web/UserController.java
+++ demo-springweb-boot/src/main/java/org/ruanwei/demo/user/web/UserController.java
@@ -1,14 +1,16 @@
 package org.ruanwei.demo.user.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("/user/")
 public class UserController {
 
-	@GetMapping("/hello")
+	@GetMapping("hello")
 	public Mono<String> sayHelloWorld() {
 		return Mono.just("Hello World");
 	}
