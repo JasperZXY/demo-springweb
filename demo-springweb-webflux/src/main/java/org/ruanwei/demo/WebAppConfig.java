5043b80fbd91286008645a85e82f0c01c8e22f13
ruanwei <ruanwei@10.12.1.49>
2018-07-15 16:33:05 +0800

    Signed-off-by: ruanwei <ruanwei@10.12.1.49>

--- demo-springweb-webflux/src/main/java/org/ruanwei/demo/WebAppConfig.java
+++ demo-springweb-webflux/src/main/java/org/ruanwei/demo/WebAppConfig.java
@@ -1,9 +1,16 @@
 package org.ruanwei.demo;
 
+import org.ruanwei.demo.user.web.UserHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 /**
  * @author ruanwei
@@ -14,4 +21,13 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @ComponentScan("org.ruanwei.demo")
 public class WebAppConfig implements WebFluxConfigurer {
 
+	@Autowired
+	private UserHandler userHandler;
+
+	@Bean
+	public RouterFunction<ServerResponse> timerRouter() {
+		return RouterFunctions.route(RequestPredicates.GET("/time"),
+				req -> userHandler.getTime(req)).andRoute(
+				RequestPredicates.GET("/date"), userHandler::getDate);
+	}
 }
